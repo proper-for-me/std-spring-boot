476620b8885bd9643c23ce505fad7e5b39252881
angun <dreamorbit@naver.com>
2021-04-29 03:50:50 +0900

    Update BootTestApp.java

--- boot-test/src/main/java/std/BootTestApp.java
+++ boot-test/src/main/java/std/BootTestApp.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackageClasses = BootTestApp.class)
 public class BootTestApp {
-
+	//원본 테스트 fork 101
 	public static void main(String[] args) {
 		SpringApplication.run(BootTestApp.class, args);
 	}
